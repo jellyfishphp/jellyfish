4821b78e0b2639b13794cedd1feaf38380e40f17
Daniel Rose <daniel-rose@gmx.de>
2018-11-20 07:54:20 +0100

    add http functionality

---
+++ packages/application/src/Jellyfish/Application/Http.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Jellyfish\Application;
+
+use Jellyfish\Kernel\KernelInterface;
+
+class Http
+{
+    /**
+     * @var \Jellyfish\Kernel\KernelInterface
+     */
+    protected $kernel;
+
+    /**
+     * @param \Jellyfish\Kernel\KernelInterface $kernel
+     */
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    public function run(): void
+    {
+        /** @var \Psr\Http\Message\ServerRequestInterface $request */
+        $request = $this->kernel->getContainer()->offsetGet('request');
+        /** @var \League\Route\Router $router */
+        $router = $this->kernel->getContainer()->offsetGet('router');
+        /** @var \Zend\HttpHandlerRunner\Emitter\EmitterInterface $emitter */
+        $emitter = $this->kernel->getContainer()->offsetGet('emitter');
+
+        $response = $router->dispatch($request);
+
+        $emitter->emit($response);
+    }
+}
