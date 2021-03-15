fb57e2e3e271856584dd4ae60a41ac3dbce809fc
Daniel Rose <daniel-rose@gmx.de>
2021-03-15 11:40:44 +0100

    Improve process bundle ⚙️ - add method to get exit code of terminated process - add method to wait for running process - add method to set timeout for running process - add method to get output (error or info) - ...

--- packages/process-symfony/tests/Jellyfish/ProcessSymfony/ProcessTest.php
+++ packages/process-symfony/tests/Jellyfish/ProcessSymfony/ProcessTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Jellyfish\ProcessSymfony;
 
 use Codeception\Test\Unit;
+use Exception;
 use Symfony\Component\Process\Process as SymfonyProcess;
 
 class ProcessTest extends Unit
@@ -67,7 +68,11 @@ class ProcessTest extends Unit
         $this->symfonyProcessMock->expects(static::never())
             ->method('start');
 
-        static::assertInstanceOf(Process::class, $this->process->start());
+        try {
+            $this->process->start();
+            static::fail();
+        } catch (Exception $exception) {
+        }
     }
 
     /**
@@ -88,5 +93,155 @@ class ProcessTest extends Unit
             ->willReturn(true);
 
         static::assertTrue($this->process->isRunning());
+    }
+
+    /**
+     * @return void
+     */
+    public function testWait(): void
+    {
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('isStarted')
+            ->willReturn(true);
+
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('wait');
+
+        static::assertInstanceOf(Process::class, $this->process->wait());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetTimeout(): void
+    {
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('getTimeout')
+            ->willReturn(1.0);
+
+        static::assertEquals(1, $this->process->getTimeout());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetTimeout(): void
+    {
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('setTimeout')
+            ->with(1.0);
+
+        static::assertInstanceOf(Process::class, $this->process->setTimeout(1));
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetOutputForError(): void
+    {
+        $errorOutput = 'Error output';
+
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('isStarted')
+            ->willReturn(true);
+
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('isSuccessful')
+            ->willReturn(false);
+
+        $this->symfonyProcessMock->expects(static::never())
+            ->method('getOutput');
+
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('getErrorOutput')
+            ->willReturn($errorOutput);
+
+        static::assertEquals($errorOutput, $this->process->getOutput());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetOutput(): void
+    {
+        $output = 'Output';
+
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('isStarted')
+            ->willReturn(true);
+
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('isSuccessful')
+            ->willReturn(true);
+
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('getOutput')
+            ->willReturn($output);
+
+        $this->symfonyProcessMock->expects(static::never())
+            ->method('getErrorOutput');
+
+        static::assertEquals($output, $this->process->getOutput());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetOutputWithoutStartedStatus(): void
+    {
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('isStarted')
+            ->willReturn(false);
+
+        $this->symfonyProcessMock->expects(static::never())
+            ->method('isSuccessful')
+            ->willReturn(true);
+
+        $this->symfonyProcessMock->expects(static::never())
+            ->method('getOutput');
+
+        $this->symfonyProcessMock->expects(static::never())
+            ->method('getErrorOutput');
+
+        try {
+            $this->process->getOutput();
+            static::fail();
+        } catch (Exception $exception) {
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetExitCode(): void
+    {
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('isTerminated')
+            ->willReturn(true);
+
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('getExitCode')
+            ->willReturn(0);
+
+        static::assertEquals(0, $this->process->getExitCode());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetExitCodeWithoutTerminatedStatus(): void
+    {
+        $this->symfonyProcessMock->expects(static::atLeastOnce())
+            ->method('isTerminated')
+            ->willReturn(false);
+
+        $this->symfonyProcessMock->expects(static::never())
+            ->method('getExitCode');
+
+        try {
+            $this->process->getExitCode();
+            static::fail();
+        } catch (Exception $exception) {
+        }
     }
 }
